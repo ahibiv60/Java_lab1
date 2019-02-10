4e9d241171ce7355ec6b65293aa0f43b6585f0e1
Bogdan.Rak <bodua55@gmail.com>
2019-02-10 12:13:41 +0200

    Initial commit

---
+++ Lab #1/src/com/demo/Perforator.java
@@ -0,0 +1,84 @@
+package com.demo;
+
+public class Perforator 
+{
+	// Основні поля
+	public int numberOfRevolutions; 				// Кількість обертів
+	public int frequencyOfStrokes;   				// Частота ударів
+	public String color;      						// Колір
+	public String producer;          				// Виробник
+	
+	// Додаткові поля
+	private String name;              				// Назва
+	private int price;        		  				// Ціна
+	protected int weight;							// Вага
+	protected String typeOfCartridge;				// Тип патрона
+	public static int power;						// Потужність
+	
+	///////////////////////////////////////Конструктори////////////////////////////////////////////////
+	
+	public Perforator(){
+		//Дефолтний конструктор
+	}
+	
+	public Perforator(int numberOfRevolutions, int frequencyOfStrokes, String color, String producer)
+	{
+		this.numberOfRevolutions = numberOfRevolutions;
+		this.frequencyOfStrokes = frequencyOfStrokes;
+		this.color = color;
+		this.producer = producer;
+	}
+	
+	public Perforator(int numberOfRevolutions, int frequencyOfStrokes, String color, String producer, String name, int price, int weight, String typeOfCartridge, int power)
+	{
+		this.numberOfRevolutions = numberOfRevolutions;
+		this.frequencyOfStrokes = frequencyOfStrokes;
+		this.color = color;
+		this.producer = producer;
+		this.name = name;
+		this.price = price;
+		this.weight = weight;
+		this.typeOfCartridge = typeOfCartridge;
+		this.power = power;
+	}
+	//////////////////////////////////////Сетери/гетери////////////////////////////////////////////////////
+	public String getName() 
+	{
+		return this.name;
+    }
+	public void setPrice(int price) 
+   	{
+		this.price=price;
+    }
+	public int getPrice() 
+	{
+		return this.price;
+    }
+	//////////////////////////////////////////Методи//////////////////////////////////////////////////
+	public String toString()
+	{
+		return String.format("Назва: %s\nКолір: %s\nВиробник: %s\nКількість обертів: %d\nЧастота ударів: %d\nВага: %d\nТип патрона: %s\nПотужність: %d\nЦіна: %d\n", name, color, producer, numberOfRevolutions, frequencyOfStrokes, weight, typeOfCartridge, power, price);
+	}
+	public static void printStaticPower()
+	{
+		System.out.println("Потужність: "+power);
+	}
+	public void printPower()
+	{
+		System.out.println("Потужність: "+power);
+	}
+	public void resetValues(int numberOfRevolutions, int frequencyOfStrokes, String color, String producer, String name, int price, int weight, String typeOfCartridge, int power)
+	{
+		this.numberOfRevolutions = numberOfRevolutions;
+		this.frequencyOfStrokes = frequencyOfStrokes;
+		this.color = color;
+		this.producer = producer;
+		this.name = name;
+		this.price = price;
+		this.weight = weight;
+		this.typeOfCartridge = typeOfCartridge;
+		this.power = power;
+	}
+}
+
+
---
+++ Lab #1/src/com/demo/PerforatorDemo.java
@@ -0,0 +1,26 @@
+package com.demo;
+
+public class PerforatorDemo {
+
+	public static void main(String[] args) 
+	{
+		Perforator perforator_1 = new Perforator();
+		Perforator perforator_2 = new Perforator(12000, 4600, "White", "Makita");
+		Perforator perforator_3 = new Perforator(11000, 4500, "Black", "Makita", "HR2470", 4099, 2, "SDS+", 780);
+		
+		System.out.println("Перфоратор №1");
+		System.out.println(perforator_1.toString());
+		System.out.println("Перфоратор №2");
+		System.out.println(perforator_2.toString());
+		System.out.println("Перфоратор №3");
+		System.out.println(perforator_3.toString());
+		
+		perforator_1.printStaticPower();
+		perforator_2.printStaticPower();
+		perforator_3.printStaticPower();
+		
+		perforator_1.printPower();
+		perforator_2.printPower();
+		perforator_3.printPower();
+	}
+}
